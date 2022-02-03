8234cdd820aa12f2eb51631d140bf23fd5b053cc
Atiksh Srivastava <31950686+Atiksh04@users.noreply.github.com>
2022-02-04 01:58:30 +0530

    worker changes

---
+++ summary.worker.js
@@ -0,0 +1,11 @@
+export default function filter(list){
+    let arr = [1,2,3,4,5];
+    let ratingObj = {};
+    arr.forEach((v)=>{
+        ratingObj[v] = list.filter((val)=> parseInt(val['overall']) === v).length;
+    });
+
+    let uniqueReviwers = list.map(v => v['reviewerName']).filter((val, index, array) => array.indexOf(val) === index);
+
+    return {"rating": ratingObj, "uniqueReviewers": uniqueReviwers};
+}
